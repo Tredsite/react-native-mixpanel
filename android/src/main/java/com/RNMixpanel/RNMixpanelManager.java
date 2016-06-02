8ad626f658d49dd0ea561bdcacd9f4dada4c0ed2
Alex Leventer <alexleventer@gmail.com>
2016-06-01 18:25:27 -0700

    fix callback

--- android/src/main/java/com/RNMixpanel/RNMixpanelManager.java
+++ android/src/main/java/com/RNMixpanel/RNMixpanelManager.java
@@ -129,8 +129,8 @@ public class RNMixpanelManager extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public String getDistinctId(Callback callback) {
-        callback(mixpanel.getDistinctId());
+    public void getDistinctId(Callback callback) {
+        callback.invoke(mixpanel.getDistinctId());
     }
 
     /**
